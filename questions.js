85c4d11ca33701cafcdf77d920f971d648c1d8ea
Velber <goncalvesvelber@gmail.com>
2024-11-07 21:52:46 -0300

    *

---
+++ questions.js
@@ -0,0 +1,53 @@
+export default[
+    {
+        question: "1",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: false },
+            { option: "3", correct: false },
+        ],
+    },
+
+    {
+        question: "2",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+        ],
+    },
+
+    {
+        question: "3",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+        ],
+    },
+
+    {
+        question: "4",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+        ],
+    },
+    {
+        question: "5",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+        ],
+    },
+    {
+        question: "6",
+        answers: [
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+            { option: "3", correct: true },
+        ],
+    },
+]
